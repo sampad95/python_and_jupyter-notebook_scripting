0baac18ea2d812307ba16a3e31d12825025ad53b
SamapdMandal <sampadmandals@gmail.com>
2022-04-17 18:21:14 +0530

    Delete sum_of_even_natural-numbers.py

--- defining_function/sum_of_even_natural-numbers.py
+++
@@ -1,7 +0,0 @@
-def sum(n):
-    sum=0
-    for i in range(1,n+1):                                # takes values upto n starting from 1
-        sum=sum+(2*i)
-    return sum
-n=eval(input('Enter how many numbers do you want to sum\n'))
-print(sum(n))
