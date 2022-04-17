369a5c116d1d8380894bf52dad9f555d0e19bf73
SamapdMandal <sampadmandals@gmail.com>
2022-04-17 18:20:49 +0530

    Add files via upload

---
+++ defining_function/sum_of_odd_integers-2.py
@@ -0,0 +1,8 @@
+def sum(n):
+    sum=0
+    for i in range(0,n+1):                                # takes values upto n starting from 0
+        if i%2!=0:
+            sum=sum+i
+    return sum
+n=eval(input('Enter the number upto which you want to sum up\n'))
+print(sum(n))
