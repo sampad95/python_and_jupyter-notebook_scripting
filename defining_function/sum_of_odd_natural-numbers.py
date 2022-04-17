16e7482118591ac887bb0cac36ce947837059b0e
SamapdMandal <sampadmandals@gmail.com>
2022-04-17 18:21:49 +0530

    Delete sum_of_odd_natural-numbers.py

--- defining_function/sum_of_odd_natural-numbers.py
+++
@@ -1,7 +0,0 @@
-def sum(n):
-    sum=0
-    for i in range(0,n):                                # takes values upto n starting from 1
-        sum=sum+(2*i+1)
-    return sum
-n=eval(input('Enter how many numbers do you want to sum\n'))
-print(sum(n))
