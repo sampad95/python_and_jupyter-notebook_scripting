f5d756c0c63430b889bcff007f113448766bcca1
sampad <sampadmandals@gmail.com>
2022-04-18 19:26:26 +0530

    Delete open-2.py

--- built-in_functions/open_function/open-2.py
+++
@@ -1,5 +0,0 @@
-file=input('enter the file name\n')
-with open(file,'r') as fw:      # Here file is a variable
-    data = fw.read()            # reads the whole file and store as 'data'.
-    print(data)                 # prints the whole content of the file opened
-    fw.close()                  # closes the file opened
