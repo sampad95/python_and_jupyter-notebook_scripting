211808350902486b319511ac1e5f17f7e094c946
mandal_sampad <sampadmandals@gmail.com>
2023-06-05 05:38:42 +0530

    Update exp_x-2.py

--- defining_function/exp_x-2.py
+++ defining_function/exp_x-2.py
@@ -2,7 +2,7 @@ from math import *
 def exp(x,n):
     sum1=0
     for j in range(n+1):
-        sum1=sum1+((x**j)/factorial(j)) # takes sum upto x^n/n!
+        sum1+=((x**j)/factorial(j)) # takes sum upto x^n/n!
     return sum1
 x=eval(input('Enter the value of x\n'))
 n=eval(input('Enter the value of n\n'))
