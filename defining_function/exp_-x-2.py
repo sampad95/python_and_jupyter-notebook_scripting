95d72aab50c2c2b4518351f5b57613bf67494cef
mandal_sampad <sampadmandals@gmail.com>
2023-06-05 05:45:29 +0530

    Update exp_-x-2.py

--- defining_function/exp_-x-2.py
+++ defining_function/exp_-x-2.py
@@ -5,5 +5,5 @@ def exp_(x,n):
         sum1=sum1+(((-1)**j)*(x**j)/factorial(j))
     return sum1
 x=eval(input('Enter the value of x\n'))
-n=eval(input('Enter the value of n\n')
+n=eval(input('Enter the value of n\n'))
 print(exp_(x,n))
