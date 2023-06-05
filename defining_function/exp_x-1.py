35c718807759a1498d294ccc911f5478bf18fb29
mandal_sampad <sampadmandals@gmail.com>
2023-06-05 05:38:05 +0530

    Update exp_x-1.py

--- defining_function/exp_x-1.py
+++ defining_function/exp_x-1.py
@@ -1,17 +1,17 @@
-def sum(x,n):
+def exp(x,n):
     """This is exp(x) infinite series sum""" 
-    sum=0
+    sum1=0
     def fact(n):                       # function inside a function
         """Factorial function inside exp(x) infinite sum function"""
         fact=1
         for i in range(1,n+1):
             fact=fact*i
         return fact
-    print(fact.__doc__)      # prints about the factorial function written inside triple quotes inside the function definition
-    for j in range(0,n+1):
-        sum=sum+((x**j)/fact(j))
-    return sum
-print(sum.__doc__)               # prints about the sum function written inside triple quotes inside the function definition
+#    print(fact.__doc__)      # prints about the factorial function written inside triple quotes inside the function definition
+    for j in range(n+1):
+        sum1+=((x**j)/fact(j)) # takes sum upto x^n/n!
+    return sum1
+#print(sum.__doc__)               # prints about the sum function written inside triple quotes inside the function definition
 x=eval(input('Enter the value of x\n'))
 n=eval(input('Enter the value of n\n'))
-print(sum(x,n))
+print(exp(x,n))
