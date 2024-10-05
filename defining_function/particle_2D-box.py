02b28f1ffcb28c79f34151bf6ec685f567a91ac1
sampad mandal     ................................................           সম্পদ মণ্ডল <sampadmandals@gmail.com>
2024-10-05 14:58:14 +0530

    Add files via upload

--- defining_function/particle_2D-box.py
+++ defining_function/particle_2D-box.py
@@ -6,17 +6,26 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
-def psi_n(a2, b2, nx, ny, x, y):
-#     a2 = eval(input("Enter xmax\n"))
-#     b2 = eval(input("Enter ymax\n"))
-#     nx = eval(input("Enter nx\n"))
-#     ny = eval(input("Enter nx\n"))
+def psi_nx_ny(a2, b2, nx, ny, x, y):
     
     lx = a2 - 0
     ly = b2 - 0
     if type(nx) == int and type(ny) == int and nx != 0 and ny != 0:
         psi = math.sqrt(4/lx*ly)*math.sin((nx*math.pi*x)/lx)*math.sin((ny*math.pi*y)/ly)
         return psi
+        
+    else:
+        print("Wrong input")
+        
+        
+def pd_nx_ny(a2, b2, nx, ny, x, y):
+    
+    lx = a2 - 0
+    ly = b2 - 0
+    
+    if type(nx) == int and type(ny) == int and nx != 0 and ny != 0:
+        psi = math.sqrt(4/lx*ly)*math.sin((nx*math.pi*x)/lx)*math.sin((ny*math.pi*y)/ly)
+        return psi**2
         
     else:
         print("Wrong input")
@@ -34,21 +43,38 @@ xmax = 1
 ymax = 1
 step = 0.01
 
-qnx = 3
-qny = 3
+qnx = 2
+qny = 2
+
+# X,Y = np.meshgrid(xvec(xmax, step), yvec(ymax, step)) # grid of point
+# PSI = np.array([np.array([psi_nx_ny(xmax, ymax, qnx, qny, i, j) for i in  xvec(xmax, step)]) 
+#                 for j in yvec(ymax, step)]) 
+
+# fig = plt.figure()
+# ax = fig.add_subplot(projection = "3d")
+# surf = ax.plot_surface(X, Y, PSI, rstride=1, cstride=1, 
+#                       cmap=cm.hsv,linewidth=0, antialiased=False)
+
+
 
 X,Y = np.meshgrid(xvec(xmax, step), yvec(ymax, step)) # grid of point
-PSI = np.array([np.array([psi_n(xmax, ymax, qnx, qny, i, j) for i in  xvec(xmax, step)]) 
-                for j in yvec(ymax, step)]) 
-
+PD = np.array([np.array([pd_nx_ny(xmax, ymax, qnx, qny, i, j) for i in  xvec(xmax, step)]) 
+                for j in yvec(ymax, step)])
 
 fig = plt.figure()
 ax = fig.add_subplot(projection = "3d")
-surf = ax.plot_surface(X, Y, PSI, rstride=1, cstride=1, 
+surf = ax.plot_surface(X, Y, PD, rstride=1, cstride=1, 
                       cmap=cm.hsv,linewidth=0, antialiased=False)
+
+
+
 
 ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+ax.set_xlabel('$X$', fontsize=20)
+ax.set_ylabel('$Y$', fontsize=20)
+
 
 fig.colorbar(surf, shrink=0.5, aspect=5)
 
